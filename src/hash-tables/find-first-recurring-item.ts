f7a4515f9b6c40c9dc8058d894f7e995ccf2fa39
Gabriel Alexandre <gabriel.shaad@gmail.com>
2022-03-23 19:46:59 -0300

    Add hash table exercises

---
+++ src/hash-tables/find-first-recurring-item.ts
@@ -0,0 +1,22 @@
+function findFirstRecurringItem(input: Array<number>) {
+  for (let index = 0; index < input.length; index++) {
+    for (let j = index + 1; j < input.length; j++) {
+      if (input[index] === input[j]) {
+        console.log(input[index]);
+        return input[index];
+      }
+    }
+  }
+}
+
+function simplifiedFindFirstRecurringItem(input: Array<number>) {
+  const map = {};
+
+  for (let index = 0; index < input.length; index++) {
+    if (map[input[index]]) {
+      return map[input[index]];
+    } else {
+      map[input[index]] = input[index];
+    }
+  }
+}
