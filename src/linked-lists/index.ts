290c8a1fbdeb7d96acf4b666d75c1c9c3f698e4e
Gabriel Alexandre <gabriel.shaad@gmail.com>
2022-03-26 21:10:48 -0300

    Add reverse linked list exercise

--- src/linked-lists/index.ts
+++ src/linked-lists/index.ts
@@ -102,4 +102,26 @@ class LinkedList {
 
     return this.printList();
   }
+
+  reverse() {
+    if (!this.head.next) {
+      return this.head;
+    }
+
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      first = second as LinkedListNode;
+      second = temp as LinkedListNode;
+    }
+
+    this.head.next = null;
+    this.head = first;
+
+    return this.printList();
+  }
 }
